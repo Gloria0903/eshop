4e00082bc5b10140caef1e636f90958ed5b68fd7
Gloria0903 <geenjeru@gmail.com>
2023-11-23 21:24:36 +0000

    first commit

---
+++ eshopping/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def detail(request):
+    return render(request, 'detail.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
